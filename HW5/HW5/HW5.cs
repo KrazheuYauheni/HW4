b50bfdcc70e37031007b177a91402954e3beb341
Nucus31 <Nucus31@gmail.com>
2022-04-27 16:00:14 +0300

    Выполнил задание 5.2

--- HW5/HW5/HW5.cs
+++ HW5/HW5/HW5.cs
@@ -10,6 +10,7 @@ namespace HW5
 			string sentence = Console.ReadLine();
 			string [] massive = ConvertStrToMas(sentence);
 			PrintWords(massive);
+			PrintWords2(ReversWords2(sentence));
 		}
 		/// <summary>
 		/// Возвражает массив слов, которые разделены пробелом
@@ -31,5 +32,53 @@ namespace HW5
 				Console.WriteLine(word);
 			}
 		}
-}
+		/// <summary>
+		/// Меняет порядок элеиентов в массиве на обратный
+		/// </summary>
+		/// <param name="inputPhrase"></param>
+		/// <returns></returns>
+		static string[] ReversWords(string inputPhrase)
+        {
+			string[] words = ConvertStrToMas(inputPhrase);
+			//Array.Reverse(words);
+	        //return words;
+			string[] revPharase = new string[words.Length];
+			for (int i = 0; i < words.Length; i++)
+            {
+				revPharase[i] = words[words.Length-1-i];
+            }
+			return revPharase;
+        }
+		/// <summary>
+		/// Меняет порядок элеиентов в массиве на обратный(другой вариант)
+		/// </summary>
+		/// <param name="inputPhrase"></param>
+		/// <returns></returns>
+		static string[] ReversWords2(string inputPhrase)
+		{
+			string[] words = ConvertStrToMas(inputPhrase);
+			//Array.Reverse(words);
+			//return words;
+			string[] revPharase = new string[words.Length];
+			int j = 0;
+			for (int i = words.Length - 1; i >= 0; i--)
+			{
+				revPharase[j] = words[i];
+				j++;
+			}
+			return revPharase;
+		}
+		/// <summary>
+		/// Выводит массив в обратном порядке
+		/// </summary>
+		/// <param name="massive"></param>
+		static void PrintWords2(string[] massive)
+		{
+			foreach (string word in massive)
+			{
+				Console.Write($"{word} ");
+			}
+		}
+		
+	}
 }
